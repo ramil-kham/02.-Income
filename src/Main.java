d29ed975b9ae21fb41d30b88db7649446a7e2665
ramil-kham <ramil66-2010@mail.ru>
2021-02-18 21:41:15 +0300

    Print Income

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int sum = 1000;
+        int interestRate = 3;
+        int income = sum * interestRate / 100;
+        System.out.println(income);
+    }
+}

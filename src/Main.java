ea99833229dd3791aa49ecfa5cf98fadf8a7e457
ramil-kham <ramil66-2010@mail.ru>
2021-02-21 19:37:42 +0300

    interestRate изменил на percent

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         int sum = 1000;
-        int interestRate = 3;
-        int income = sum * interestRate / 100;
+        int percent = 3;
+        int income = sum * percent / 100;
         System.out.println(income);
     }
 }
